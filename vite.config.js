6f93173bcbb54b303ab54afa910fb9f5ab599d4b
red <tachatz214@gmail.com>
2022-09-06 14:01:38 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/html-templater/',
-    publicDir: './src/assets',
     plugins: [vue()],
     resolve: {
         alias: {
