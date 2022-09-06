2ed30803e1001d81e99d3d445e5edb20848137ce
red <tachatz214@gmail.com>
2022-09-06 14:55:09 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,8 @@ export default defineConfig({
     },
     define: {
         global: 'globalThis',
-        'process.env': '{}'
+        'process.env': '{}',
+        emitter: 'function() {}'
     },
 
     build: {
