271908f817b7398b963b622ab67aaf4957564fd1
red <tachatz214@gmail.com>
2022-09-06 15:11:30 -0500

    fix

--- gh_pages_deploy.js
+++ gh_pages_deploy.js
@@ -3,7 +3,7 @@ import { execa } from 'execa';
 import fs from 'fs';
 (async function() {
     try {
-        try { await execa('git', ['branch', '-D', 'gh-pages'])} }
+        try { await execa('git', ['branch', '-D', 'gh-pages']) }
         catch(e) {}
         await execa('git', ['checkout', '--orphan', 'gh-pages']);
         // eslint-disable-next-line no-console
